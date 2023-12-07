0a92d55877f3ef06819a767d79b968b74630c1c8
jason <sophiehoka@gmail.com>
2023-12-07 19:30:29 +0300

    homepage update

--- MyProject/Journique/urls.py
+++ MyProject/Journique/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import pin_list, pin_detail, add_pin, home
+from .views import pin_list, pin_detail, add_pin, home, view_1, view_2, view_3, view_4, view_5
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('home/', home, name='home'),
     path('pins/', pin_list, name='pin_list'),
     path('pin/<int:pin_id>/', pin_detail, name='pin_detail'),
     path('pin/add/', add_pin, name='add_pin'),
+    path('view_1/', view_1, name='view_1'),
+    path('view_2/', view_2, name='view_2'),
+    path('view_3/', view_3, name='view_3'),
+    path('view_4/', view_4, name='view_4'),
+    path('view_5/', view_5, name='view_5'),
 ]
--- MyProject/Journique/views.py
+++ MyProject/Journique/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Pin
 from .forms import PinForm
+from django.http import HttpResponse
 
 
 def home(request):
@@ -31,3 +32,28 @@ def add_pin(request):
         form = PinForm()
 
     return render(request, 'add_pin.html', {'form': form})
+
+
+def view_1(request):
+    if request.method == 'POST':
+        return HttpResponse("View 1 clicked")
+
+
+def view_2(request):
+    if request.method == 'POST':
+        return HttpResponse("View 2 clicked")
+
+
+def view_3(request):
+    if request.method == 'POST':
+        return HttpResponse("View 3 clicked")
+
+
+def view_4(request):
+    if request.method == 'POST':
+        return HttpResponse("View 4 clicked")
+
+
+def view_5(request):
+    if request.method == 'POST':
+        return HttpResponse("View 5 clicked")
