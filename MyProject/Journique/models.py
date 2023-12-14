2a20dfc8362632414588ee22069660c5b91c63cf
jason <sophiehoka@gmail.com>
2023-12-14 22:07:24 +0300

    postgresql involved

--- MyProject/Journique/models.py
+++ MyProject/Journique/models.py
@@ -12,13 +12,13 @@ class UserProfile(models.Model):
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
-    if created:
+    if created and not hasattr(instance, 'userprofile'):
         UserProfile.objects.create(user=instance)
-
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.userprofile.save()
+    if hasattr(instance, 'userprofile'):
+        instance.userprofile.save()
 
 
 class Category(models.Model):
--- MyProject/MyProject/settings.py
+++ MyProject/MyProject/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'MyProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'journiquedb',
+        'USER': 'postgres',
+        'PASSWORD': '23085634',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
