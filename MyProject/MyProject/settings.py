cecc92da9790b32a5b402abd42fdeca3eacc7a72
jason <sophiehoka@gmail.com>
2024-01-11 15:13:42 +0300

    Add static files and index.html for GitHub Pages

--- MyProject/MyProject/settings.py
+++ MyProject/MyProject/settings.py
@@ -137,7 +137,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Journique/static'),
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = Path(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
